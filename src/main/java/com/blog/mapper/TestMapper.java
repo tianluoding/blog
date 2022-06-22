f99334dea5edf68fcd93208f3d798bff5fe5bbb7
tld <tianluoding@qq.com>
2022-06-22 16:28:19 +0800

    mybatis-generator逆向生成

---
+++ src/main/java/com/blog/mapper/TestMapper.java
@@ -0,0 +1,31 @@
+package com.blog.mapper;
+
+import com.blog.domain.Test;
+import com.blog.domain.TestExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface TestMapper {
+    int countByExample(TestExample example);
+
+    int deleteByExample(TestExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Test record);
+
+    int insertSelective(Test record);
+
+    List<Test> selectByExample(TestExample example);
+
+    Test selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Test record, @Param("example") TestExample example);
+
+    int updateByExample(@Param("record") Test record, @Param("example") TestExample example);
+
+    int updateByPrimaryKeySelective(Test record);
+
+    int updateByPrimaryKey(Test record);
+}
