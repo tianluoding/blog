4d8195c4a2418cfe6de89f7a5c3e77011f98eee0
tld <tianluoding@qq.com>
2022-06-22 14:42:27 +0800

    集成热部署，下次编译重新启动会快很多

--- src/main/java/com/blog/BlogApplication.java
+++ src/main/java/com/blog/BlogApplication.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @Slf4j
