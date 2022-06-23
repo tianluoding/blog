a596642726f6b0a1a19f3a8b588800a45f8f5578
tld <tianluoding@qq.com>
2022-06-23 12:51:27 +0800

    构建Vue3项目

---
+++ web/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <a-layout>
+    <the-header></the-header>
+    
+    <router-view/>
+      
+    <a-layout-footer style="text-align: center">
+      TLD BLOG ©2022 
+    </a-layout-footer>
+  </a-layout>
+</template>
+
+<script lang="ts">
+import TheHeader from '@/components/the-header.vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'app',
+  components: {
+    TheHeader,
+  },
+});
+</script>
+
+<style>
+#components-layout-demo-top-side .logo {
+  float: left;
+  width: 120px;
+  height: 31px;
+  margin: 16px 24px 16px 0;
+  background: rgba(255, 255, 255, 0.3);
+}
+
+.ant-row-rtl #components-layout-demo-top-side .logo {
+  float: right;
+  margin: 16px 0 16px 24px;
+}
+
+.site-layout-background {
+  background: #fff;
+}
+</style>
