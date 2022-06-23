78706b919f374f0fe0c4645cdffe3f1d01be3ddc
tld <tianluoding@qq.com>
2022-06-23 13:30:09 +0800

    跨域配置

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -55,12 +55,18 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import axios from 'axios';
 // import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'HomeView',
-  components: {
-    // HelloWorld,
-  },
+  setup() {
+    console.log("setup")
+    axios.get("http://localhost/ebook/list").then(
+      (repsonse) => {
+        console.log(repsonse);
+      }
+    )
+  }
 });
 </script>
