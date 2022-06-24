8f657f252d59f882b45384d5e4d119ff7b026f9a
tld <tianluoding@qq.com>
2022-06-24 11:40:14 +0800

    增加消息转换器解决前端Long类型字段精度丢失问题

--- src/main/java/com/blog/config/WebConfig.java
+++ src/main/java/com/blog/config/WebConfig.java
@@ -1,9 +1,14 @@
 package com.blog.config;
 
+import com.blog.util.JacksonObjectMapper;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -16,7 +21,20 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowCredentials(true)
                 .maxAge(3600); // 1小时内不需要再预检（发OPITIONS请求）
     }
-//    @Autowired
+
+    /**
+     * 扩展消息转换器
+     * @param converters
+     */
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+
+        messageConverter.setObjectMapper(new JacksonObjectMapper());
+
+        converters.add(0, messageConverter);
+    }
+    //    @Autowired
 //    LogInterceptor logInterceptor;
 //
 //    @Override
